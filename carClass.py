b39402e563329e81564c2f5aeee00917e1a6c1e8
trsavi <vukasin.vasiljevic@gmail.com>
2021-11-17 20:01:14 +0100

    added more parameters

---
+++ carClass.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 17 19:42:00 2021
+
+@author: Vukasin
+"""
+
+
+import pandas as pd
+
+data = pd.read_csv('./Data/carsPolovni.csv')
+
+def get_brands():
+    return list(data['Brend'].unique())
+
+def get_models(brend):
+    return list(data[data['Brend']==brend]['Model'].unique())
+
+def get_car_types(model):
+    return list(data[data['Model']==model]['Karoserija'].unique())
+
+def get_car_volume(model):
+    return sorted(list(data[data['Model']==model]['Kubikaza'].unique()))
+
+def get_fuel_type(model, karoserija):
+    return list(data[(data['Model']==model) & (data['Karoserija']==karoserija)]['Gorivo'].unique())
+
+def get_engine_power(model, kubikaza):
+    return list(data[(data['Model']==model) & (data['Kubikaza']==kubikaza)]['Snaga'].unique())
--- mainApp.py
+++ mainApp.py
@@ -8,6 +8,7 @@ Author: Vukasin
 """
 
 import streamlit as st
+import carClass
 
 ### Info
 
@@ -18,10 +19,21 @@ st.write("Prediction is based on car specification that you can enter in left si
          
 st.sidebar.write(""" # Car specifications """)
 
+
 ## Entering car specification
 
-brand = st.sidebar.selectbox("Select Brand", ("Mazda","Audi", "BMW"))
+brand = st.sidebar.selectbox("Select Brand", carClass.get_brands())
 
-model = st.sidebar.selectbox("Select Model", ("308","C4", "508"))
+model = st.sidebar.selectbox("Select Model", carClass.get_models(brand))
+
+car_type = st.sidebar.selectbox("Select car type", carClass.get_car_types(model))
+
+car_volume = st.sidebar.selectbox("Select engine volume", carClass.get_car_volume(model))
+
+car_volume = st.sidebar.selectbox("Select fuel type", carClass.get_fuel_type(model, car_type))
 
 mileage = st.sidebar.slider('Enter mileage',50000,250000)
+
+power = st.sidebar.slider('Enter engine power',60,250)
+
+
